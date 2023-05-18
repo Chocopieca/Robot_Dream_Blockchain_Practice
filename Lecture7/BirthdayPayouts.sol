3cea05032b3d23f911ce779ff18a302eedb8c84c
Kopyl Maksym <m.s.kopyl.kharkov@gmail.com>
2023-05-18 03:26:48 +0300

    study: Add home work 7.

--- Lecture7/BirthdayPayouts.sol
+++ Lecture7/BirthdayPayouts.sol
@@ -1,80 +1,76 @@
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 import "./BokkyPooBahsDateTimeLibrary.sol";
 
 contract BirthdayPayout {
-
-// store name,address teammates
-// get name, addresses of teammates
-// send ether to address teammates on command only by owner
-
     string _name;
-
-    address _owner;
-
-    Teammate[] public _teammates;
+    address public _owner;
+    uint256 public _createDate;
+    Teammate[] private _teammates;
 
     struct Teammate {
         string name;
         address account;
         uint256 salary;
         uint256 birthday;
+        uint256 dateLastGift;
     }
 
-    constructor() public {
+    constructor() payable {
         _name="max";
         _owner = msg.sender;
+        _createDate = block.timestamp;
     }
 
     function addTeammate(address account,string memory name, uint256 salary,uint256 birthday) public onlyOwner {
         require(msg.sender != account,"Cannot add oneself");
-        Teammate memory newTeammate = Teammate(name,account,salary, birthday);
+        Teammate memory newTeammate = Teammate(name,account,salary, birthday, 0);
         _teammates.push(newTeammate);
         emit NewTeammate(account,name);
     }
 
-    function findBirthday() public onlyOwner returns(uint256){
-        // it is a good idea to check whether therea are any teammates in the database
+    function findBirthday() public onlyOwner{
         require(getTeammatesNumber()>0,"No teammates in the database");
-        // what are loops: https://www.youtube.com/watch?v=GwcisLY5avc
-        // so basically we iterate over every Teammate in the _teammates array...
         for(uint256 i=0;i<getTeammatesNumber();i++){
-        // and check their birthday
-            if(checkBirthday(i)){
-            // if the birthday is today - we return the user
-                return uint256(i);
+            if(checkGift(i)){
+                uint256 today = block.timestamp;
+                birthdayPayout(i);
+                _teammates[i].dateLastGift = today;
             }
         }
-        // this will revert the transaction if no teammates with birthday are found
-        revert("Noone found");
     }
 
-    // this function is instead of sendToTeammate
-    function birthdayPayout(uint256 index) public onlyOwner {
-    	// send some money to the teammate
+    function birthdayPayout(uint256 index) private onlyOwner {
         sendToTeammate(index);
-        // and emit a HeppyBirthday event(just in case)
         emit HappyBirthday(_teammates[index].name,_teammates[index].account);
     }
 
-    function getDate(uint256 timestamp) view public returns(uint256 year, uint256 month, uint256 day){
-        (year, month, day) = BokkyPooBahsDateTimeLibrary.timestampToDate(timestamp);
+    function sendToTeammate(uint256 index) public onlyOwner {
+        payable(_teammates[index].account).transfer(_teammates[index].salary);
     }
 
-    function checkBirthday(uint256 index) view public returns(bool){
-        // day & month of today's date == day & month of birthday teammate
-        //block.timestamp = 1671560040
-        //birthday = 877126202
-        //BokkyPooBahsDateTimeLibrary.timestampToDate();
+    function checkGift(uint256 index) private view returns (bool) {
         uint256 birthday = getTeammate(index).birthday;
-        (, uint256 birthday_month,uint256 birthday_day) = getDate(birthday);
-        uint256 today = block.timestamp;
-        (, uint256 today_month,uint256 today_day) = getDate(today);
+        uint256 lastGift = getTeammate(index).dateLastGift;
 
-        if(birthday_day == today_day && birthday_month==today_month){
-            return true;
-        }
-        return false;
+        return isDateInPast(birthday) && !isDateInThisYear(lastGift);
+    }
+
+    function isDateInPast(uint256 date) view private returns(bool) {
+        uint256 today = block.timestamp;
+        return today > date;
+    }
+
+    function isDateInThisYear(uint256 date) view private returns(bool) {
+        uint256 today = block.timestamp;
+        (uint256 date_year,,) = getDate(date);
+        (uint256 today_year,,) = getDate(today);
+        return date_year == today_year;
+    }
+
+    function getDate(uint256 timestamp) pure private returns(uint256 year, uint256 month, uint256 day){
+        (year, month, day) = BokkyPooBahsDateTimeLibrary.timestampToDate(timestamp);
     }
 
     function getTeammate(uint256 index) view public returns(Teammate memory){
@@ -85,16 +81,12 @@ contract BirthdayPayout {
         return  _teammates;
     }
 
-    function getTeammatesNumber() view public returns(uint256){
+    function getTeammatesNumber() view private returns(uint256){
         return _teammates.length;
     }
 
-    function sendToTeammate(uint256 index) public onlyOwner{
-        payable(_teammates[index].account).transfer(_teammates[index].salary);
-    }
-
-    function deposit() public payable{
-
+    function getTimestamp() view public returns(uint256){
+        return  block.timestamp;
     }
 
     modifier onlyOwner{
