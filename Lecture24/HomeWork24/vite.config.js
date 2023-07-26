989691e270696d6ad40fe0a8bc2d6d98b48f2875
Kopyl Maksym <m.s.kopyl.kharkov@gmail.com>
2023-07-26 12:17:22 +0300

    pages: Update vite config

--- Lecture24/HomeWork24/vite.config.js
+++ Lecture24/HomeWork24/vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
   return {
+    base: "/Robot_Dream_Blockchain_Practice/",
     plugins: [vue()],
     resolve: {
       alias: {
