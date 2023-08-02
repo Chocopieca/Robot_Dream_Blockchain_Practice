53f31b25d95f766402a43acffd1814f143af7cb5
Kopyl Maksym <m.s.kopyl.kharkov@gmail.com>
2023-08-03 00:43:45 +0300

    'Update_dist'

--- assets/BaseDivider-f791b616.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,K as a,o as s,a as r,R as t}from"./index-12d66c63.js";const n=a({name:"BaseDivider",props:{color:{type:String,default:"#FFF"}}});function d(e,_,c,i,l,p){return s(),r("div",{class:"header-divider",style:t({background:e.color})},null,4)}const f=o(n,[["render",d],["__scopeId","data-v-ed700744"]]);export{f as default};
