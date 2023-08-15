c35477fc512abdd28a71cad68c5b8d1f8e3d901e
Kopyl Maksym <m.s.kopyl.kharkov@gmail.com>
2023-08-15 21:17:13 +0300

    'Update_dist'

---
+++ assets/AppHeader-901b0419.js
@@ -0,0 +1,4 @@
+
+
+
+import{_ as n,r as _,o as c,a as r,e as s,g as t,w as o,f as a}from"./index-1b8bad95.js";const d={name:"AppHeader"},p={class:"container-fluid"},l={class:"container w-100"};function i(u,f,m,h,v,x){const e=_("router-link");return c(),r("header",p,[s("nav",l,[s("div",null,[t(e,{to:"eth"},{default:o(()=>[a("ETH")]),_:1}),t(e,{to:"erc20"},{default:o(()=>[a("ERC20")]),_:1}),t(e,{to:"btc"},{default:o(()=>[a("BTC")]),_:1})])])])}const B=n(d,[["render",i],["__scopeId","data-v-95789234"]]);export{B as default};
