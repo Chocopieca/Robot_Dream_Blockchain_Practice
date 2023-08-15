9e9e600fe4e5d6cd1f1f9cb8da7df3c5778bcc4d
MaksymKopyl <m.s.kopyl.kharkov@gmail.com>
2023-08-15 16:35:38 +0300

    Update_dist

--- Lecture24/HomeWork24/dist/assets/BaseDivider-07ccc53c.js
+++
@@ -1,4 +0,0 @@
-
-
-
-import{_ as o,u as a,o as s,a as r,x as t}from"./index-e395a367.js";const n=a({name:"BaseDivider",props:{color:{type:String,default:"#FFF"}}});function d(e,_,c,i,l,p){return s(),r("div",{class:"header-divider",style:t({background:e.color})},null,4)}const f=o(n,[["render",d],["__scopeId","data-v-ed700744"]]);export{f as default};
