839f0fce4a729879e5eba82d9b18787292c44e41
Kopyl Maksym <m.s.kopyl.kharkov@gmail.com>
2023-08-15 20:47:28 +0300

    'Update_dist'

--- Lecture24/HomeWork24/dist/assets/BaseCard-8c83ffc0.js
+++
@@ -1,4 +0,0 @@
-
-
-
-import{_ as s,u as o,o as t,a as r,e as n,v as d,x as a}from"./index-78a8234e.js";const l=o({name:"BaseCard",props:{width:{type:String},bgColor:{type:String}}});function c(e,i,p,_,u,f){return t(),r("div",{class:"bh-card",style:a({maxWidth:e.width?`${e.width}px`:"100%"})},[n("div",{class:"bh-card-inner",style:a({backgroundColor:e.bgColor})},[d(e.$slots,"default",{},void 0,!0)],4)],4)}const h=s(l,[["render",c],["__scopeId","data-v-241a5a3e"]]);export{h as default};
