e3a17414d88584a2ecce790b2de35e0166283cba
Kopyl Maksym <m.s.kopyl.kharkov@gmail.com>
2023-08-15 18:09:37 +0300

    'Update_dist'

---
+++ Lecture24/HomeWork24/dist/assets/BaseLoading-3e74d79a.js
@@ -0,0 +1,4 @@
+
+
+
+import{_ as d,u as a,o as s,a as e,I as t}from"./index-78a8234e.js";const o=a({name:"BaseLoading"}),c={class:"loader"},i=t('<div class="loader__body" data-v-d7918e65><div class="sk-chase" data-v-d7918e65><div class="sk-chase-dot" data-v-d7918e65></div><div class="sk-chase-dot" data-v-d7918e65></div><div class="sk-chase-dot" data-v-d7918e65></div><div class="sk-chase-dot" data-v-d7918e65></div><div class="sk-chase-dot" data-v-d7918e65></div><div class="sk-chase-dot" data-v-d7918e65></div></div></div>',1),v=[i];function _(n,l,r,p,h,k){return s(),e("div",c,v)}const u=d(o,[["render",_],["__scopeId","data-v-d7918e65"]]);export{u as default};
