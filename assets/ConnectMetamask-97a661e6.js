f958a99a70d54c4ceb3f0cfd4956ad2200454c74
MaksymKopyl <m.s.kopyl.kharkov@gmail.com>
2023-08-15 04:20:47 +0300

    Update_dist

--- assets/ConnectMetamask-97a661e6.js
+++
@@ -1,4 +0,0 @@
-
-
-
-import{_ as r,s as _,r as o,o as a,b as n,w as s,E as d,T as u}from"./index-16cfe48a.js";import{u as m}from"./useEtherJsStore-cbfd4020.js";import{u as p}from"./useErc20TokenStore-82c493ef.js";const l=_({name:"ConnectMetamask",data(){return{isLoading:!1}},setup(){const t=p();async function e(){await t.init()}return{initErc20:e}},methods:{async onConnectMetamask(){this.isLoading=!0,await m().onConnect(),await this.loadData(),this.isLoading=!1},async loadData(){await this.initErc20()}}});function f(t,e,k,C,h,B){const c=o("BaseLoading"),i=o("BaseButton");return a(),n(u,{mode:"out-in"},{default:s(()=>[t.isLoading?(a(),n(c,{key:0})):(a(),n(i,{key:1,buttonColor:"#E95420",class:"connect-button",onClick:t.onConnectMetamask},{default:s(()=>[d(" Connect Metamask ")]),_:1},8,["onClick"]))]),_:1})}const E=r(l,[["render",f],["__scopeId","data-v-520a34aa"]]);export{E as default};
