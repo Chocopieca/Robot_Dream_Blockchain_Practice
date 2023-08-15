80520c279836aafc1b621c8a1b0a4feec6f1ea0c
Kopyl Maksym <m.s.kopyl.kharkov@gmail.com>
2023-08-15 20:47:28 +0300

    'Update_dist'

---
+++ assets/BaseCard-a56b5515.js
@@ -0,0 +1,4 @@
+
+
+
+import{_ as s,u as o,o as t,a as r,e as n,v as d,x as a}from"./index-259e7a3e.js";const l=o({name:"BaseCard",props:{width:{type:String},bgColor:{type:String}}});function c(e,i,p,_,u,f){return t(),r("div",{class:"bh-card",style:a({maxWidth:e.width?`${e.width}px`:"100%"})},[n("div",{class:"bh-card-inner",style:a({backgroundColor:e.bgColor})},[d(e.$slots,"default",{},void 0,!0)],4)],4)}const h=s(l,[["render",c],["__scopeId","data-v-241a5a3e"]]);export{h as default};
--- assets/BaseDivider-5d65c024.js
+++
@@ -1,4 +0,0 @@
-
-
-
-import{_ as o,u as a,o as s,a as r,x as t}from"./index-78a8234e.js";const n=a({name:"BaseDivider",props:{color:{type:String,default:"#FFF"}}});function _(e,c,d,i,l,p){return s(),r("div",{class:"header-divider",style:t({background:e.color})},null,4)}const f=o(n,[["render",_],["__scopeId","data-v-676e998e"]]);export{f as default};
